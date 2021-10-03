fd218d483061c31803c5365f4431f30d6b05f807
Dananando <dan.andriamihaja@gmail.com>
2021-10-03 16:19:51 +0200

    Before dark mode toggle

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
 // import logo from '../assets/logo.svg';
+import { useState } from 'react';
 import '../css/App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from '../themes';
 
 import Aside from './Aside';
 import Main from './Main';
 
+const StyledApp = styled.div`
+`;
+
 function App() {
+
+  const [theme, setTheme] = useState('light');
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  }
   return (
-    <div className="App">
-      <Aside />
-      <Main />
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        <div className="App">
+          <Aside />
+          <Main />
+        </div>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
